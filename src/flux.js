471c52c17e53b2e3cdd4309bfb0fdde49760c74e
yes-xodnd <taew3167@gmail.com>
2021-07-19 18:27:05 +0900

    Update JSDoc comments

--- src/flux.js
+++ src/flux.js
@@ -3,39 +3,68 @@ const deepClone = obj => JSON.parse(JSON.stringify(obj));
 const dispatcher = (() => {
   const stores = [];
   const dispatcher = {
-    register(store) {
-      stores.push(store);
-      return store;
+    /**
+     * 디스패처에 스토어를 등록합니다.
+     * 스토어의 dispatch 함수를 인자로 받습니다.
+     * @param {Function} dispatchStore 
+     */
+    register(dispatchStore) {
+      stores.push(dispatchStore);
     },
+    /**
+     * 등록된 스토어에 액션을 디스패치합니다.
+     * 액션 객체를 인자로 받습니다.
+     * @param {Object} action 
+     */
     dispatch(action) {
-      stores.forEach(store => store.dispatch(action));
+      stores.forEach(dispatchStore => dispatchStore(action));
     }
   };
 
-  return Object.freeze(dispatcher);
+  return dispatcher;
 })();
 
+export const { dispatch } = dispatcher;
+
+/**
+ * 초기화 상태와 리듀서 함수를 받아 스토어를 생성합니다.
+ * @param {*} initState
+ * @param {Function} reducer 
+ * @returns 
+ */
 export const createStore = (initState, reducer = () => {}) => {
   let state = initState;
   const subscribers = [];
-  const emit = () => subscribers
-    .forEach(component => component.render());
+
+  /**
+   * 액션 객체를 전달받고, 리듀서로 상태를 업데이트합니다.
+   * 스토어를 구독하는 컴포넌트를 리렌더링 합니다.
+   * @param {Object} action 
+   */
+  const dispatch = action => {
+    state = reducer(state, action);
+    subscribers.forEach(component => component.render());
+  }
+  dispatcher.register(dispatch);
 
   const store = {
+    /**
+     * state의 사본을 반환합니다.
+     * @returns state
+     */
     getState() {
       return deepClone(state);
     },
+    /**
+     * 스토어에 컴포넌트를 등록합니다.
+     * 등록한 컴포넌트는 스토어가 업데이트할 때마다 리렌더링합니다.
+     * @param {*} component 
+     */
     subscribe(component) {
       subscribers.push(component);
     },
-    dispatch(action) {
-      state = reducer(state, action);
-      emit();
-    }
   };
 
-  return Object.freeze(dispatcher.register(store));
+  return Object.freeze(store);
 }
-
-export const dispatch = dispatcher.dispatch;
 
