6f50af73638658b643f7defbe5165faed76ed67f
yes-xodnd <taew3167@gmail.com>
2021-07-19 18:28:00 +0900

    Add actionTypes.js to separate and reuse action types

--- src/stores/todo.js
+++ src/stores/todo.js
@@ -1,4 +1,11 @@
 import { createStore } from '../flux.js';
+import { 
+  ADD_TODO,
+  DELETE_TODO,
+  TOGGLE_TODO_DONE,
+  CLEAR_TODO,
+  CLEAR_TODO_DONE
+} from '../actionTypes.js';
 
 const initState = {
   todos: [],
@@ -6,26 +13,26 @@ const initState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       state.todos = [ action.data.todoItem, ...state.todos ];
       return state;
 
-    case 'DELETE_TODO':
+    case DELETE_TODO:
       state.todos = state.todos.filter(item => item.id !== action.data.id);
       return state;
     
-    case 'TOGGLE_TODO_DONE':
+    case TOGGLE_TODO_DONE:
       {
         const index = state.todos.findIndex(item => item.id === action.data.id);
         state.todos[index].done = !state.todos[index].done;
       }
       return state;
     
-    case 'CLEAR_TODO':
+    case CLEAR_TODO:
       state.todos = [];
       return state;
     
-    case 'CLEAR_TODO_DONE':
+    case CLEAR_TODO_DONE:
       state.todos = state.todos.filter(item => !item.done);
       return state;  
     
